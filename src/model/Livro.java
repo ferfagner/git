786fe2c0e3aa7e3aaddde3135df728cc36c8eb59
marcos.guimaraes <marcos.guimaraes@AN012016MM077117.dinfor.uniceub>
2017-10-19 20:51:27 -0200

    Commit inicial

---
+++ src/model/Livro.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Livro {
+	//Atributos
+	private String titulo;
+	private String autor;
+	private String editora;
+	private String anoLancamento;
+	private String ISBN;
+	
+	public Livro(String titulo, String autor, String editora, String anoLancamento, String iSBN) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editora = editora;
+		this.anoLancamento = anoLancamento;
+		ISBN = iSBN;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public String getAnoLancamento() {
+		return anoLancamento;
+	}
+	public void setAnoLancamento(String anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	
+	
+
+}
